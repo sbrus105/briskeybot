0c45a44394245b0247e84a7d5d4e68d09da1e5d9
Sam B <samuelbrus@gmail.com>
2019-05-16 23:33:14 -0500

    Better comments

--- app.py
+++ app.py
@@ -52,7 +52,7 @@ def send_message(msg):
 #5d108f5841a9977ddfa018e014  	The Deuce
 #44730fb274b2b3538d071f6f84		Tester
   data = {
-          'bot_id' : '5d108f5841a9977ddfa018e014',
+          'bot_id' : '5d108f5841a9977ddfa018e014', #replace this code with above ones for corresponding GM
           'text'   : msg,
          }
   request = Request(url, urlencode(data).encode())
