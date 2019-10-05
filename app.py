feccd3dc59c6684653ab6bea4802b7dc136ddec6
Samuel Brus <samuelbrus@gmail.com>
2019-10-05 02:17:53 -0500

    Add files via upload

--- app.py
+++ app.py
@@ -62,16 +62,16 @@ def webhook():
 #---------------------------------------------------------
 
 def send_message(msg):
-   url  = 'https://api.groupme.com/v3/bots/post'
+    url  = 'https://api.groupme.com/v3/bots/post'
   
-#5d108f5841a9977ddfa018e014  	The Deuce
-#44730fb274b2b3538d071f6f84		Tester
-data = {
-        'bot_id' : 'disabled', #replace this code with above ones for corresponding GM
+    #5d108f5841a9977ddfa018e014  	The Deuce
+    #44730fb274b2b3538d071f6f84		Tester
+    data = {
+        'bot_id' : '5d108f5841a9977ddfa018e014', #replace this code with above ones for corresponding GM
         'text'   : msg,
        }
-request = Request(url, urlencode(data).encode())
-json = urlopen(request).read().decode()
+    request = Request(url, urlencode(data).encode())
+    json = urlopen(request).read().decode()
 
 
 def log(msg):
